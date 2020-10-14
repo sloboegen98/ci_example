31fdad6a857d6771b5a7cbe2133dab16981b1f02
sloboegen98 <sloboegen98@ya.ru>
2020-10-15 01:15:48 +0300

    init

---
+++ mult.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def mult(a, b):
+    return np.dot(a, b)
---
+++ test.py
@@ -0,0 +1,28 @@
+import unittest
+import numpy as np
+from mult import mult
+
+
+class TestDot(unittest.TestCase):
+    def test1(self):
+        n = 3
+        a = np.ones((n, n))
+        b = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+        self.assertEquals(mult(a, b), np.dot(a, b))
+
+    def test2(self):
+        a = 1010
+        b = 12
+
+        self.assertEquals(mult(a, b), np.dot(a, b))
+
+    def test3(self):
+        a = [1, 2, 3, 4]
+        b = [5, 6, 7, 8]
+
+        self.assertEquals(mult(a, b), np.dot(a, b))
+        
+
+if __name__ == '__main__':
+    unittest.main()
